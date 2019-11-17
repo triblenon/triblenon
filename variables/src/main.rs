e8c10f0a4bde8af4ef128a26692f0df9fa037729
Win Myat Oo <winmyatoo@Wins-MacBook-Pro.local>
2019-11-17 22:27:01 +0630

    Learning variables in progress

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut y = 5;
+    println!("The value of x is: {}", y);
+    y = 6;
+    println!("The value of x is: {}", y);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+}
