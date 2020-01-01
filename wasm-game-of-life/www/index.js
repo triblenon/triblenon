de540456656e34c4312fc9ad269a98c787bda6f8
Win Myat Oo <winmyatoo@Wins-MacBook-Pro.local>
2020-01-01 19:47:19 +0630

    started adding time profiling

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -24,7 +24,7 @@ canvas.width = (CELL_SIZE + 1) * width + 1;
 const ctx = canvas.getContext('2d');
 
 const renderLoop = () => {
-
+  fps.render();
   universe.tick();
 //  debugger;
   drawGrid();
@@ -117,11 +117,63 @@ canvas.addEventListener("click", event => {
   const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
   const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
 
+//  console.log(boundingRect);
+//  console.log(canvas);
+//  console.log(scaleX);
+//  console.log(scaleY);
+//  console.log(canvasLeft);
+//  console.log(canvasTop);
+//  console.log(row);
+//  console.log(col);
+
   universe.toggle_cell(row, col);
 
   drawGrid();
   drawCells();
 });
+
+const fps = new class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = 1 / delta * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+  }
+};
 
 drawGrid();
 drawCells();
